39564af123bca945f0c6b68b37f8bf5e2771a137
Juliana <152028539+ju-liana-cf@users.noreply.github.com>
2023-12-14 18:09:22 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const stopwatch = document.getElementById('stopwatch');
+const playPauseButton = document.getElementById('play-pause');
+
+let stopwatchInterval;
+let runningTime = 0;
+
+const playPause = () => {
+    const isPaused = !playPauseButton.classList.contains('running');
+    if (isPaused) {
+        playPauseButton.classList.add('running');
+        start();
+    } else {
+        playPauseButton.classList.remove('running');
+        pause();
+    }
+}
+
+const pause = () => {
+    clearInterval(stopwatchInterval);
+}
+
+const stop = () => {
+    playPauseButton.classList.remove('running');
+    runningTime = 0;
+    clearInterval(stopwatchInterval);
+    stopwatch.textContent = '00:00';
+}
+
+const start = () => {
+    let startTime = Date.now() - runningTime;
+    stopwatchInterval = setInterval(() => {
+        runningTime = Date.now() - startTime;
+        stopwatch.textContent = calculateTime(runningTime);
+    }, 1000)
+}
+
+const calculateTime = runningTime => {
+    const total_seconds = Math.floor(runningTime / 1000);
+    const total_minutes = Math.floor(total_seconds / 60);
+
+    const display_seconds = (total_seconds % 60).toString().padStart(2, "0");
+    const display_minutes = total_minutes.toString().padStart(2, "0");
+
+    return `${display_minutes}:${display_seconds}`
+}
